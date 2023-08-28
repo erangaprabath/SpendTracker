07649501166d29caa958fa5cd7f130b198edc609
Eranga Prabath <97217265+erangaprabath@users.noreply.github.com>
2023-08-28 18:33:48 +0530

    Add files via upload
    main

---
+++ SpendTracker/SpendTracker/SpendTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendTrackerApp.swift
+//  SpendTracker
+//
+//  Created by Eranga prabath on 2023-08-28.
+//
+
+import SwiftUI
+
+@main
+struct SpendTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
